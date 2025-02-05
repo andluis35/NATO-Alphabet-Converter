3057b104ffe29c4fd0146d2823dea1d2bf833656
Anderson Luis <and.luis35@gmail.com>
2025-02-05 10:53:46 -0300

    Adicionada personalização na interação com usuário

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import pandas
+import art
+import time
+
+def print_header():
+    print(art.logo)
+
+def print_loading_screen():
+    print("Converting", end='')
+    for _ in range(3):
+        print(".", end='')
+        time.sleep(1)
+
+def clear_screen():
+    print("\n" * 50)
 
 def get_encoded_letter(key, code_dictionary):
     return code_dictionary[key]
@@ -19,12 +33,19 @@ def delete_spaces(string):
     return string.replace(" ", "")
 
 def print_encoded_word(word):
+    print("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+")
     print(f"RESULT: {word}")
+    print("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+")
 
 def start_encoding():
+    print_header()
     phonetic_alphabet = set_phonetic_alphabet_dictionary()
     chosen_word = input("Type a word to convert: ")
     chosen_word = delete_spaces(chosen_word)
+
+    clear_screen()
+    print_loading_screen()
+    clear_screen()
 
     word_in_phonetic = encode_word(chosen_word, phonetic_alphabet)
     print_encoded_word(word_in_phonetic)
